b6ef2789d8960b3de63c2b3f9cec79cd8b323a7d
Joao Ricardo <seuemail@gmail.com>
2026-05-22 23:57:27 -0300

    initial commit

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,15 +1,43 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Reference reference = new Reference("John", 3, 16);
+        List<Scripture> scriptures = new List<Scripture>();
 
-        Scripture scripture = new Scripture(
-            reference,
-            "For God so loved the world that he gave his only begotten Son"
+        scriptures.Add(
+            new Scripture(
+                new Reference("John", 3, 16),
+                "For God so loved the world that he gave his only begotten Son."
+            )
         );
+
+        scriptures.Add(
+            new Scripture(
+                new Reference("Éter", 12, 18),
+                "And neither at any time hath any wrought miracles until after their faith."
+            )
+        );
+
+        scriptures.Add(
+            new Scripture(
+                new Reference("Alma", 37, 6),
+                "By small and simple things are great things brought to pass, and small means in many instances doth confound the wise."
+            )
+        );
+
+        scriptures.Add(
+            new Scripture(
+                new Reference("Proverbs", 3, 5),
+                "Trust in the Lord with all your heart and lean not on your own understanding."
+            )
+        );
+
+        Random random = new Random();
+        int index = random.Next(scriptures.Count);
+        Scripture scripture = scriptures[index];
 
         while (!scripture.IsCompletelyHidden())
         {
@@ -18,7 +46,7 @@ class Program
             Console.WriteLine(scripture.GetDisplayText());
 
             Console.WriteLine();
-            Console.WriteLine("Press ENTER to continue or type quit");
+            Console.WriteLine("Press ENTER to continue or type quit:");
 
             string input = Console.ReadLine();
 
