e352a7fdfc24facde48cce2e1a54cce18fb6772b
Joao Ricardo <seuemail@gmail.com>
2026-05-22 23:17:54 -0300

    week 03

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+
+        string[] splitWords = text.Split(" ");
+
+        foreach (string word in splitWords)
+        {
+            _words.Add(new Word(word));
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        string result = _reference.GetDisplayText() + " ";
+
+        foreach (Word word in _words)
+        {
+            result += word.GetDisplayText() + " ";
+        }
+
+        return result;
+    }
+
+    public void HideRandomWords()
+    {
+        Random random = new Random();
+
+        for (int i = 0; i < 3; i++)
+        {
+            int index = random.Next(_words.Count);
+
+            if (!_words[index].IsHidden())
+            {
+                _words[index].Hide();
+            }
+        }
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+
+    
