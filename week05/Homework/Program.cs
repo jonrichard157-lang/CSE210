b95e9489b5e068eadd1a550fd21700398e8fd85a
Joao Ricardo <seuemail@gmail.com>
2026-06-06 00:37:28 -0300

    commit initial

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,29 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment1 = new Assignment("Samuel Bennett", "Multiplication");
+
+        Console.WriteLine(assignment1.GetSummary());
+
+
+        MathAssignment math = new MathAssignment(
+        "Roberto Rodriguez",
+        "Fractions",
+        "7.3",
+        "8-19"
+        );
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+        WritingAssignment writing = new WritingAssignment(
+        "Mary Waters",
+        "European History",
+        "The Causes of World War II"
+        );
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
     }
+
+
 }
